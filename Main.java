5f3ad9f3c131b6395923d8f001e3e0f3fcb8d263
Rici Rahman <rici7rahman@gmail.com>
2021-10-22 12:39:20 +0700

    first commit

---
+++ Main.java
@@ -0,0 +1,49 @@
+class Main {
+    public static void main(String[] args) {
+      Heroes hero = new Heroes();
+  
+      hero.setProfile("Rici", 19, 165);
+      hero.profile();
+      
+  
+      // jalankan aktivitas disini...
+      hero.jalan();
+      hero.jalan();
+      hero.jalan();
+      hero.jalan();
+      hero.jalan();
+  
+      hero.lari();
+      hero.lari();
+      hero.lari();
+      hero.lari();
+
+      hero.makan();
+      hero.makan();
+      hero.makan();
+      hero.makan();
+
+      hero.minum();
+      hero.minum();
+      hero.minum();
+      hero.minum();
+
+      hero.lompat();
+      hero.lompat();
+      hero.lompat();
+
+      hero.duduk();
+      hero.duduk();
+      
+
+
+
+
+
+
+      
+      // tampilkan status terkini...
+      hero.profile();
+      
+    }
+  }
